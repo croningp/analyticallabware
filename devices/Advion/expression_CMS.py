2d1f5414f16c60a37d3fe59f628c0bf7a1d36fcc
S. Hessam M. Mehr <Hessam.Mehr@glasgow.ac.uk>
2018-09-10 19:30:17 +0100

    Initial commit.
    Basic support for Advion expression CMS based on Jarek's code.

---
+++ devices/Advion/expression_CMS.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+# !/usr/bin/env python
+"""
+"IKA_microstar_75" -- API for IKA microstar 7.5 remote controllable overhead stirrer
+====================================================================================
+
+.. module:: expression_CMS
+   :platform: Windows
+   :synopsis: Advion expression CMS mass spectrometer.
+   :license: BSD 3-clause
+.. moduleauthor:: S. Hessam M. Mehr <Hessam.Mehr@chem.gla.ac.uk>
+.. moduleauthor:: Jaroslaw Granda <Jaroslaw.Granda@chem.gla.ac.uk>
+
+(c) 2018 The Cronin Group, University of Glasgow
+
+This provides a python class for the IKA microstar 7.5 overhead stirrer based on software developed by Stefan Glatzel.
+The command implementation is based on the German manual pages 32-34.
+
+For style guide used see http://xkcd.com/1513/
+"""
+
+import logging
+
+from advion import (AcquisitionManager, AcquisitionState, ErrorCode,
+                    InstrumentController, InstrumentState, get_Instrument)
+from os.path import dirname, join
+from time import sleep
+
+class AdvionCMS:
+    def __init__(self, ion_source='positiveAPCI.ion', method='default.method', tune='currentcalibESIpositive102016.tune'):
+        filedir = join(dirname(__file__), 'files')
+        self.ion_source = open(join(filedir, ion_source))
+        self.method = open(join(filedir, method))
+        self.tune = open(join(filedir, tune))
+        self.controller = InstrumentController
+        self.acqmgr = AcquisitionManager
+
+    def __enter__(self):
+        logging.info(f'Entering {self.__class__.__name__}')
+        # Initialiaze mass spec controller
+        err = self.controller.startController()
+        if err != ErrorCode.CMS_OK:
+            raise ValueError(f'Cannot initialize controller (got {err})')
+        # Check if mass spec can operate
+        while not self.controller.canOperate():
+            logging.info('Waiting for ms')
+            sleep(1)
+        # Put mass spec into operate mode
+        self.controller.operate()
+        return self
+
+    def measure_ms(self, name, path):
+        logging.info(f'Acquiring {name} => {path}')
+        err = self.acqmgr.start(self.method, self.ion_source, self.tune, name, path)
+        if err != ErrorCode.CMS_OK:
+            raise ValueError(f'Cannot measure (got {err})')
+        logging.info('Waiting for ms to finish acquisition')
+        sleep(1)
+        logging.info(self.acqmgr.getState())
+        while self.acqmgr.getState() == AcquisitionState.Underway:
+            sleep(1)
+        sleep(2)
+        logging.info(self.acqmgr.isFinalizingData())
+        logging.info(AcquisitionManager.getState())
+        logging.info(f'Finished acquisition {name} => {path}')
+
+    def isAcquisitionUnderway(self):
+        return self.acqmgr.getState() == AcquisitionState.Underway
+
+    def __exit__(self, errortype, value, traceback):
+        logging.info(f'Exiting {self.__class__.__name__}')
+        #check if ms can be put into standby mode
+        while not self.controller.canStandby():
+            time.sleep(0.1)
+        #puts ms into standby mode
+        self.controller.standby()
+        #stop mscontroller
+        self.controller.stopController()
+
+if __name__ == '__main__':
+    # testing code
+    pass
