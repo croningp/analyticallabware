de589bead8851f14af8e434f772276b0b3e9ca76
Hessam Mehr <hessam.mehr@glasgow.ac.uk>
2020-06-30 16:51:09 +0100

    Update init to include hplc

--- AnalyticalLabware/__init__.py
+++ AnalyticalLabware/__init__.py
@@ -4,3 +4,4 @@ from .devices.OceanOptics.Raman.raman_control import OceanOpticsRaman
 from .devices.OceanOptics.UV.QEPro2192 import QEPro2192
 from .devices.OceanOptics.IR.NIRQuest512 import NIRQuest512
 from .devices.DrDAQ.pH_module import DrDaqPHModule
+from .devices.Agilent.hplc import HPLCController
