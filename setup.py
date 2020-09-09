ed00b35bcc700113e64d5b1214673d5720a050a4
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2020-09-09 18:08:52 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnalyticalLabware",
-    version="0.2.1",
+    version="0.2.2",
     description="Analytical instruments for the Chemputer",
     url="https://gitlab.com/croningroup/chemputer/analyticallabware",
-    author="Hessam Mehr, Artem Leonov, Graham Keenan",
+    author="Hessam Mehr, Artem Leonov, Graham Keenan, Alex Hammer",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "scipy",
-        "numpy",
-        "matplotlib",
         "SerialLabware"
     ],
     zip_safe=False
