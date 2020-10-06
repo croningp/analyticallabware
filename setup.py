6dac3a5cc9a23edf3f64e8e6b365e92f1caeadec
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2020-10-06 16:00:46 +0100

    Requirements moved to setup

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnalyticalLabware",
-    version="0.2.2",
+    version="0.2.3",
     description="Analytical instruments for the Chemputer",
     url="https://gitlab.com/croningroup/chemputer/analyticallabware",
     author="Hessam Mehr, Artem Leonov, Graham Keenan, Alex Hammer",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "SerialLabware"
+        "SerialLabware @ git+ssh://git@gitlab.com/croningroup/chemputer/seriallabware.git",
+        "scipy",
+        "matplotlib",
+        "seabreeze",
+        "numpy",
+        "nmrglue",
     ],
     zip_safe=False
 )
