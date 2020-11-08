f343ae995983b42ed84988f8d8fb212dbaabda09
Alex Hammer <2423935H@student.gla.ac.uk>
2020-11-08 22:51:03 +0000

    SL2 implementation of IDEX valve now in SerialLabware. Fix imports.

--- AnalyticalLabware/devices/IDEX/mxii_valve.py
+++ AnalyticalLabware/devices/IDEX/mxii_valve.py
@@ -1,8 +1,7 @@
 import SerialLabware
 
 try:
-    from SerialLabware.controllers import AbstractDistributionValve
     # running SerialLabware 2
-    from .mxii_valve_sl2 import IDEXMXIIValve
+    from SerialLabware import IDEXMXIIValve
 except ImportError:
     from .mxii_valve_sl1 import IDEXMXIIValve
