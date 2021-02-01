0912e23937a5751c65fd8d697a4da676061de390
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2021-02-01 19:24:35 +0000

    Advion: Remove old wrapper dll and sources.

--- AnalyticalLabware/devices/Advion/build.bat
+++
@@ -1,5 +0,0 @@
-set INCLUDE_DIR="..\AdvionAPI.6.0.11.3.VS10\include"
-set LIB_DIR="..\AdvionAPI.6.0.11.3.VS10\Release"
-set OUTPUT="advion_wrapper/advion_wrapper.dll"
-
-cl /LD /Fe%OUTPUT% /I %INCLUDE_DIR% %LIB_DIR%\*.lib src/advion_wrapper.cpp
