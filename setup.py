b9722460e82502c81be8cb2444380792f835bb08
Artem Leonov <artem.leonov@glasgow.ac.uk>
2021-03-02 16:47:39 +0000

    Fixed wrong link to chemputerapi repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnalyticalLabware",
-    version="0.2.3",
+    version="0.2.4",
     description="Analytical instruments for the Chemputer",
     url="https://gitlab.com/croningroup/chemputer/analyticallabware",
     author="Hessam Mehr, Artem Leonov, Graham Keenan, Alex Hammer",
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "SerialLabware @ git+ssh://git@gitlab.com/croningroup/chemputer/seriallabware.git",
-        "ChemputerAPI @ git+ssh://git@gitlab.com/croningroup/chemputer/seriallabware.git",
+        "ChemputerAPI @ git+ssh://git@gitlab.com/croningroup/chemputer/chemputerapi.git",
         "scipy",
         "matplotlib",
         "seabreeze",
