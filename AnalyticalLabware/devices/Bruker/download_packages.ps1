e6d0d3f61ba2edd1b1d2245c6c801dfac947d032
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2021-03-09 13:31:35 +0000

    BrukerMS: Add scripts to download and install packages on MS PC.

---
+++ AnalyticalLabware/devices/Bruker/download_packages.ps1
@@ -0,0 +1,3 @@
+# meant for Python 3.4.10 on Windows XP
+mkdir -Force packages
+pip download -r .\requirements.txt -d packages
---
+++ AnalyticalLabware/devices/Bruker/install.bat
@@ -0,0 +1,5 @@
+@REM Install downloaded packages
+
+@REM Run download_packages.ps1 on a recent version of Windows
+@REM to download required packages from PyPI.
+pip install --no-index -f packages ipython pyro4
