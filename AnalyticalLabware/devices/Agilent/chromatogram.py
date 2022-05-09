5abcb8b55325efe95a1f49085ff85f8d1a45dd0a
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2022-05-09 14:16:51 +0100

    Issued warning for working with .npz files

--- AnalyticalLabware/devices/Agilent/chromatogram.py
+++ AnalyticalLabware/devices/Agilent/chromatogram.py
@@ -2,13 +2,18 @@
 
 import logging
 import time
+import warnings
 from pathlib import Path
+from typing import TYPE_CHECKING, Union, Any
 
 import numpy as np
 
 from .chemstation import CHFile
 
 from ...analysis import AbstractSpectrum
+
+if TYPE_CHECKING:
+    import os
 
 # Chemstation data path
 DATA_DIR = r"C:\Chem32\1\Data"
@@ -21,6 +26,13 @@ ACQUISITION_PARAMETERS = "acq.txt"
 
 # format used in acquisition parameters
 TIME_FORMAT = "%Y-%m-%d-%H-%M"
+
+# file format for DAD detectors
+DAD_FILE_FORMAT = "DAD1{}"
+
+# file suffixes
+NPZ_FILE_SUFFIX = ".npz"
+CHEMSTATION_FILE_SUFFIX = ".ch"
 
 
 class AgilentHPLCChromatogram(AbstractSpectrum):
@@ -77,29 +89,56 @@ class AgilentHPLCChromatogram(AbstractSpectrum):
 
     ### PUBLIC METHODS TO LOAD RAW DATA ###
 
-    def extract_rawdata(self, experiment_dir: str, channel: str):
-        """
-        Reads raw data from Chemstation .CH files.
+    def extract_rawdata(
+        self,
+        experiment_dir: Union[str, 'os.PathLike'],
+        channel: str,
+    ) -> tuple(np.array, np.array, dict[str, Any]):
+        """ Reads raw data and metadata from Chemstation .CH files.
 
         Args:
-            experiment_dir: .D directory with the .CH files
+            experiment_dir (Union[str, os.PathLike]): .D directory with the
+                .CH files.
+            channel (str): Selected channel in the detector.
 
         Returns:
-            np.array(times), np.array(values)   Raw chromatogram data
+            tuple(np.array, np.array, dict[str, Any]): Tuple of raw
+                chromatographic data and metadata:
+                    - times (np.array): Time scale.
+                    - values (np.array): Readings from the detector.
+                    - metadata (dict[str, Any]): Dictionary with metadata,
+                        associated with the given acquisition.
+
+        Notes:
+            Only looks for DAD detector data.
         """
-        filename = Path(experiment_dir).joinpath(f"DAD1{channel}")
-        npz_file = filename.with_suffix(".npz")
+        filename = Path(experiment_dir).joinpath(
+            DAD_FILE_FORMAT.format(channel))
+        npz_file = filename.with_suffix(NPZ_FILE_SUFFIX)
 
         if npz_file.is_file():
             # already processed
-            data = np.load(npz_file)
-            return data["times"], data["values"]
-        else:
-            self.logger.debug("NPZ file not found. First time loading data.")
-            ch_file = filename.with_suffix(".ch")
-            data = CHFile(ch_file)
-            np.savez_compressed(npz_file, times=data.times, values=data.values)
-            return np.array(data.times), np.array(data.values)
+            return self._treat_npz_file(npz_filepath=npz_file)
+
+        self.logger.debug("NPZ file not found. First time loading data.")
+        ch_file = filename.with_suffix(CHEMSTATION_FILE_SUFFIX)
+        data = CHFile(ch_file)
+        np.savez_compressed(npz_file, times=data.times, values=data.values)
+        return np.array(data.times), np.array(data.values)
+
+    def _treat_npz_file(
+        self,
+        npz_filepath: Union[str, 'os.PathLike']
+    ) -> tuple(np.array, np.array, str):
+        """ Legacy method to read from .npz compressed arrays. """
+
+        warnings.warn("Manipulating .npz files is deprecated and will be \
+removed soon. Consider using save_data() and load_data() methods to work with \
+pickled files.", FutureWarning)
+
+        data = np.load(npz_filepath)
+
+        return (data['times'], data['values'])
 
     def extract_peakarea(self, experiment_dir: str):
         """
